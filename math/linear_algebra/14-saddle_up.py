319036a7c3affb47aaea14a16ab89031ef307de6
Jeremy Mitts <jermitts@gmail.com>
2024-08-29 11:55:05 -0500

    initial push for task 11

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,13 +1,1 @@
 #!/usr/bin/env python3
-"""
-Write a function def np_transpose(matrix): that transposes matrix:
-
- * You can assume that matrix can be interpreted as a numpy.ndarray
- * You are not allowed to use any loops or conditional statements
- * You must return a new numpy.ndarray
-"""
-
-
-def np_transpose(matrix):
-    """ return transposed matrix """
-    return matrix.T
