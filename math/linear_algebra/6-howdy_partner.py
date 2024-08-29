b1fd73427c8ec063a05bf41326b956d112a1cf5e
Jeremy Mitts <jermitts@gmail.com>
2024-08-29 10:54:51 -0500

    initial push for task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Write a function def cat_arrays(arr1, arr2): that concatenates two arrays:
+
+ * You can assume that arr1 and arr2 are lists of ints/floats
+ * You must return a new list
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays of integers/floats.
+
+    Args:
+        arr1 (list): The first array of integers/floats.
+        arr2 (list): The second array of integers/floats.
+
+    Returns:
+        list: A new array containing the concatenated elements of arr1 and arr2
+    """
+    return arr1 + arr2
