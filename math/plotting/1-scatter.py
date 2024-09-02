325fce127ce432c7f1b0da016f63e046e8bc0198
Jeremy Mitts <jermitts@gmail.com>
2024-09-02 14:36:07 -0500

    scatter

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+""" scatter file demo """
 import numpy as np
 import matplotlib.pyplot as plt
 
 def scatter():
-
+    """ scatter graph showing """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -11,4 +12,14 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    # your code here
+    plt.scatter(x, y, color='magenta')
+
+    # Set labels for x-axis and y-axis
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+
+    # Set title
+    plt.title("Men's Height vs Weight")
+
+    # Display the plot
+    plt.show()
