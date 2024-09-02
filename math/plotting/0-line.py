e0d7ca75e984de24af3651c904b95b08ddde3db6
Jeremy Mitts <jermitts@gmail.com>
2024-09-02 14:30:54 -0500

    pycodestyle

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+""" do a line thing in this file. """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """ make a line thing """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
