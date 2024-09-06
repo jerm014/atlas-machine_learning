b15d7904d09ec22ada9c9bcc4321d2215cb4d811
Jeremy Mitts <jermitts@gmail.com>
2024-09-06 17:51:49 -0500

    pycodestyle

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -94,38 +94,39 @@ class Normal:
         # Calculate the CDF
         return 0.5 * (1 + sign * erf_val)
 
+    @staticmethod
+    def erf(x):
+        """ approximation for erf """
+        # Constants
+        a1 =  0.254829592
+        a2 = -0.284496736
+        a3 =  1.421413741
+        a4 = -1.453152027
+        a5 =  1.061405429
+        p  =  0.3275911
 
-def erf(x):
-    """ approximation for erf """
-    # Constants
-    a1 =  0.254829592
-    a2 = -0.284496736
-    a3 =  1.421413741
-    a4 = -1.453152027
-    a5 =  1.061405429
-    p  =  0.3275911
+        # Save the sign of x
+        sign = -1
+        if x >= 0:
+            sign = 1
 
-    # Save the sign of x
-    sign = -1
-    if x >= 0:
-        sign = 1
+        x = abs(x)
 
-    x = abs(x)
+        # A&S formula 7.1.26
+        t = 1.0 / (1.0 + p * x)
+        y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * exp(-x * x)
 
-    # A&S formula 7.1.26
-    t = 1.0 / (1.0 + p * x)
-    y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * exp(-x * x)
+        return sign * y
 
-    return sign * y
-
-def exp(x):
-    """
-    Taylor series approximation for e^x
-    """
-    n = 20  # Number of terms in the series
-    result = 1.0
-    term = 1.0
-    for i in range(1, n):
-        term *= x / i
-        result += term
-    return result
+    @staticmethod
+    def exp(x):
+        """
+        Taylor series approximation for e^x
+        """
+        n = 20  # Number of terms in the series
+        result = 1.0
+        term = 1.0
+        for i in range(1, n):
+            term *= x / i
+            result += term
+        return result
