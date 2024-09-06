eb4e9deb8b0e56831f8d77abf6b8b98f3b9c572f
Jeremy Mitts <jermitts@gmail.com>
2024-09-06 17:13:57 -0500

    trying with np

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -86,24 +86,4 @@ class Normal:
         Returns:
             float: The CDF value for x.
         """
-        z = self.z_score(x)
-        e = 2.7182818285
-
-        # Use the error function approximation
-        a1 = 0.254829592
-        a2 = -0.284496736
-        a3 = 1.421413741
-        a4 = -1.453152027
-        a5 = 1.061405429
-        p = 0.3275911
-
-        sign = 1
-        if z < 0:
-            sign = -1
-        z = abs(z) / (2**0.5)
-
-        t = 1.0 / (1.0 + p * z)
-        y = (1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t *
-             e ** (-z * z))
-
-        return 0.5 * (1.0 + sign * y)
+        return float(0.5 * (1 + np.erf(self.z_score(x) / np.sqrt(2))))
