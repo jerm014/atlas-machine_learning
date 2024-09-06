8cf4fd4dee8d5ef4f6f4c44ec092f8df904b54a8
Jeremy Mitts <jermitts@gmail.com>
2024-09-06 17:48:50 -0500

    pycodestyle

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -114,8 +114,7 @@ def erf(x):
 
     # A&S formula 7.1.26
     t = 1.0 / (1.0 + p * x)
-    y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t *
-               exp(-x * x)
+    y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * exp(-x * x)
 
     return sign * y
 
