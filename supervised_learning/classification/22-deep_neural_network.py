c3020867750eefbfc53a2c6935f76b4ef1192a5c
Jeremy Mitts <jermitts@gmail.com>
2024-09-17 15:00:37 -0500

    22 second pass

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -1,85 +1,86 @@
 #!/usr/bin/env python3
-""" Module containing the DeepNeuralNetwork class for binary classification """
+"""Module containing DeepNeuralNetwork class for binary classification."""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """ Defines a deep neural network performing binary classification """
+    """Defines a deep neural network for binary classification."""
 
     def __init__(self, nx, layers):
-        """ Class constructor for the deep neural network """
+        """Initialize a DeepNeuralNetwork instance."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or not layers:
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        for ll in range(1, self.__L + 1):
-            layer_size = layers[ll-1]
-            prev_layer_size = nx if ll == 1 else layers[ll-2]
-            self.__weights[f'W{ll}'] = np.random.randn(layer_size,
-                                                       prev_layer_size) * \
-                np.sqrt(2 / prev_layer_size)
-            self.__weights[f'b{ll}'] = np.zeros((layer_size, 1))
+        for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            layer_size = layers[i]
+            input_size = nx if i == 0 else layers[i - 1]
+            self.__weights[f'W{i+1}'] = np.random.randn(
+                layer_size, input_size) * np.sqrt(2 / input_size)
+            self.__weights[f'b{i+1}'] = np.zeros((layer_size, 1))
 
     @property
     def L(self):
-        """ Getter for L """
+        """Getter for the number of layers."""
         return self.__L
 
     @property
     def cache(self):
-        """ Getter for cache """
+        """Getter for the intermediary values of the network."""
         return self.__cache
 
     @property
     def weights(self):
-        """ Getter for weights """
+        """Getter for the weights and biases of the network."""
         return self.__weights
 
     def forward_prop(self, X):
-        """ Calculates the forward propagation of the neural network """
+        """Calculate the forward propagation of the neural network."""
         self.__cache['A0'] = X
-        for ll in range(1, self.__L + 1):
-            Z = np.matmul(self.__weights[f'W{ll}'],
-                          self.__cache[f'A{ll-1}']) + self.__weights[f'b{ll}']
-            self.__cache[f'A{ll}'] = 1 / (1 + np.exp(-Z))
+        for i in range(1, self.__L + 1):
+            Z = (np.matmul(self.__weights[f'W{i}'], self.__cache[f'A{i-1}']) +
+                 self.__weights[f'b{i}'])
+            self.__cache[f'A{i}'] = 1 / (1 + np.exp(-Z))
         return self.__cache[f'A{self.__L}'], self.__cache
 
     def cost(self, Y, A):
-        """ Calculates the cost of the model using logistic regression """
+        """Calculate the cost of the model using logistic regression."""
         m = Y.shape[1]
-        cost = -1/m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -1/m * np.sum(Y * np.log(A) + (1-Y) * np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
-        """ Evaluates the neural network's predictions """
+        """Evaluate the neural network's predictions."""
         A, _ = self.forward_prop(X)
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
         return prediction, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """ Calculates one pass of gradient descent on the neural network """
+        """Calculate one pass of gradient descent on the neural network."""
         m = Y.shape[1]
         dZ = cache[f'A{self.__L}'] - Y
-        for ll in reversed(range(1, self.__L + 1)):
-            dW = 1/m * np.matmul(dZ, cache[f'A{ll-1}'].T)
-            db = 1/m * np.sum(dZ, axis=1, keepdims=True)
-            if ll > 1:
-                dZ = np.matmul(self.__weights[f'W{ll}'].T, dZ) * \
-                  (cache[f'A{ll-1}'] * (1 - cache[f'A{ll-1}']))
-            self.__weights[f'W{ll}'] -= alpha * dW
-            self.__weights[f'b{ll}'] -= alpha * db
+        for i in range(self.__L, 0, -1):
+            A_prev = cache[f'A{i-1}']
+            dW = (1/m) * np.matmul(dZ, A_prev.T)
+            db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+            dA_prev = np.matmul(self.__weights[f'W{i}'].T, dZ)
+            dZ = dA_prev * (A_prev * (1 - A_prev))
+            self.__weights[f'W{i}'] -= alpha * dW
+            self.__weights[f'b{i}'] -= alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
-        """ Trains the deep neural network """
+        """Train the deep neural network."""
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
