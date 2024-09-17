3d9c16f89729f9e06b12673aca7c0d5aaeef09e6
Jeremy Mitts <jermitts@gmail.com>
2024-09-17 14:49:00 -0500

    loops reduced to 1

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -15,14 +15,14 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(l, int) and l > 0 for l in layers):
-            raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
         for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
             layer_size = layers[i]
             input_size = nx if i == 0 else layers[i - 1]
             self.__weights[f'W{i+1}'] = np.random.randn(
