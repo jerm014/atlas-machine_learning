c46c3e5397cd75b3a5eff837441f06472c907f83
Jeremy Mitts <jermitts@gmail.com>
2024-09-17 14:47:49 -0500

    17 second pass

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,44 +1,45 @@
 #!/usr/bin/env python3
-""" Module containing the DeepNeuralNetwork class for binary classification """
+"""Module containing DeepNeuralNetwork class for binary classification."""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """ Defines a deep neural network performing binary classification """
+    """Defines a deep neural network for binary classification."""
 
     def __init__(self, nx, layers):
-        """ Class constructor for the deep neural network """
+        """Initialize a DeepNeuralNetwork instance."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or not layers:
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(l, int) and l > 0 for l in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        for ll in range(1, self.__L + 1):
-            layer_size = layers[ll-1]
-            prev_layer_size = nx if ll == 1 else layers[ll-2]
-            self.__weights[f'W{ll}'] = np.random.randn(layer_size,
-                                                       prev_layer_size) * \
-                np.sqrt(2 / prev_layer_size)
-            self.__weights[f'b{ll}'] = np.zeros((layer_size, 1))
+        for i in range(self.__L):
+            layer_size = layers[i]
+            input_size = nx if i == 0 else layers[i - 1]
+            self.__weights[f'W{i+1}'] = np.random.randn(
+                layer_size, input_size) * np.sqrt(2 / input_size)
+            self.__weights[f'b{i+1}'] = np.zeros((layer_size, 1))
 
     @property
     def L(self):
-        """ Getter for L """
+        """Getter for the number of layers."""
         return self.__L
 
     @property
     def cache(self):
-        """ Getter for cache """
+        """Getter for the intermediary values of the network."""
         return self.__cache
 
     @property
     def weights(self):
-        """ Getter for weights """
+        """Getter for the weights and biases of the network."""
         return self.__weights
