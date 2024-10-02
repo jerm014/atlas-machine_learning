1d857a27cc0cb0863f89cce9ed7c1eef1b41797d
Jeremy Mitts <jermitts@gmail.com>
2024-10-02 11:55:02 -0500

    task 0

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""create_placeholders function for project 2287 task 0"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    Creates two placeholders, x and y, for the neural network.
+
+    Args:
+        nx (int): The number of feature columns in our data.
+        classes (int): The number of classes in our classifier.
+
+    Returns:
+        tuple: Two tf.placeholder objects named x and y, respectively.
+            x is the placeholder for the input data to the neural network.
+            y is the placeholder for the one-hot labels for the input data.
+    """
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
