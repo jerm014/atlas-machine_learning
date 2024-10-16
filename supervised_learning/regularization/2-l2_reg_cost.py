7db57ed97685f04646f328f5ef2c4e27e103ea2d
Jeremy Mitts <jermitts@gmail.com>
2024-10-16 18:35:59 -0500

    not concat

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -14,9 +14,12 @@ def l2_reg_cost(cost, model):
     Returns:
     tensor containing the total cost accounting for L2 regularization
     """
-    l2_cost = 0
+    l2_costs = []
     for layer in model.layers:
         if hasattr(layer, 'kernel_regularizer') and layer.kernel_regularizer:
-            l2_cost += tf.reduce_sum(layer.losses)
-
-    return cost + l2_cost
+            l2_cost = tf.reduce_sum(layer.losses)
+            l2_costs.append(l2_cost)
+    
+    total_l2_cost = tf.reduce_sum(l2_costs)
+    out = tf.concat([tf.expand_dims(cost + total_l2_cost, 0), l2_costs], axis=0)
+    return out
