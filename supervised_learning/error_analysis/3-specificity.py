25470c6fa8f0f78eef63b658c663479218de6589
Jeremy Mitts <jermitts@gmail.com>
2024-10-16 16:34:45 -0500

    pycodestyle

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -7,14 +7,14 @@ import numpy as np
 def specificity(confusion):
     """
     calculate the specificity for each class in a confusion matrix
-    
+
     Args:
       confusion: numpy.ndarray of shape (classes, classes)
         row indices represent the correct labels
         column indices represent the predicted labels
         classes is the number of classes
     Returns:
-      numpy.ndarray of shape (classes,) containing the specificity of each class
+      numpy.ndarray of shape (classes,) containing specificity of each class
 
     Notes from PLD:
       Specificity is true negative rate tn / tn + fp (minimize fp)
@@ -25,12 +25,12 @@ def specificity(confusion):
     specificity = np.zeros(classes)
     # calculate the specificity for each class
     for i in range(classes):
-      # get the true negative rate
-      true_negatives = np.sum(confusion) - np.sum( \
-        confusion[i, :]) - np.sum(confusion[:, i]) + confusion[i, i]
-      # get the false positive rate
-      false_positives = np.sum(confusion[:, i]) - confusion[i, i]
-      # calculate the specificity
-      specificity[i] = true_negatives / (true_negatives + false_positives)
+        # get the true negative rate
+        true_negatives = np.sum(confusion) - np.sum( \
+          confusion[i, :]) - np.sum(confusion[:, i]) + confusion[i, i]
+        # get the false positive rate
+        false_positives = np.sum(confusion[:, i]) - confusion[i, i]
+        # calculate the specificity
+        specificity[i] = true_negatives / (true_negatives + false_positives)
 
     return specificity
