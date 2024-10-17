66793d2d05dce7f8749cbb070834bfc02751bb7e
Jeremy Mitts <jermitts@gmail.com>
2024-10-16 19:52:19 -0500

    pycodestyle

--- supervised_learning/regularization/13-batch_norm.py
+++ supervised_learning/regularization/13-batch_norm.py
@@ -8,14 +8,14 @@ def batch_norm(Z, gamma, beta, epsilon):
   """
     # Calculate mean of each feature
     mu = np.mean(Z, axis=0)
-    
+
     # Calculate variance of each feature
     var = np.var(Z, axis=0)
-    
+
     # Normalize Z 
     Z_norm = (Z - mu) / np.sqrt(var + epsilon)
     
     # Scale and shift the normalized values
     Z_tilde = gamma * Z_norm + beta
-    
+
     return Z_tilde
