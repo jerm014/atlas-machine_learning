de40ec89435c4d8d7499245972934776ed740f62
Jeremy Mitts <jermitts@gmail.com>
2024-10-22 15:11:47 -0500

    0

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" module to build a keras model """
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.regularizers import l2
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network with Keras.
+
+    Args:
+        nx (int):           Number of input features to the network.
+        layers (list):      List containing the number of nodes in each layer.
+        activations (list): List containing the activation functions for each
+                            layer.
+        lambtha (float):    L2 regularization parameter.
+        keep_prob (float):  Probability that a node will be kept during dropout.
+
+    Returns:
+        keras.Model: The constructed Keras model.
+    """
+    model = Sequential()
+    for i in range(len(layers)):
+        if i == 0:
+            # Add the first layer with input shape
+            model.add(Dense(units=layers[i],
+                            activation=activations[i],
+                            kernel_regularizer=l2(lambtha),
+                            input_shape=(nx,)))
+        else:
+            # Add subsequent layers
+            model.add(Dense(units=layers[i],
+                            activation=activations[i],
+                            kernel_regularizer=l2(lambtha)))
+        if i < len(layers) - 1:
+            # Add dropout after each layer except the last
+            model.add(Dropout(rate=1 - keep_prob))
+    return model
--- supervised_learning/regularization/pictures.py
+++ supervised_learning/regularization/pictures.py
@@ -58,4 +58,56 @@ def lasso_regression(X, y, alpha, n_iterations=1000, learning_rate=0.01):
     
     return weights
 
-    
+  def early_stopping(cost, opt_cost, threshold, patience, count):
+    if cost < opt_cost - threshold:
+        count = 0
+    else:
+        count += 1
+
+    return count >= patience, count
+
+
+    @tf.function
+    def add_noise(x):
+        noise = tf.random.normal(shape=tf.shape(x), mean=0.0, stddev=0.1)
+        return x + noise
+
+    @tf.function
+    def random_scale(x):
+        scale = tf.random.uniform(shape=(), minval=0.8, maxval=1.2)
+        return x * scale
+
+    @tf.function
+    def random_rotate(x):
+        angle = tf.random.uniform(shape=(), minval=-np.pi/4, maxval=np.pi/4)
+        rotation_matrix = tf.stack([
+            [tf.cos(angle), -tf.sin(angle)],
+            [tf.sin(angle), tf.cos(angle)]
+        ])
+        return tf.matmul(tf.reshape(x, [-1, 2]), rotation_matrix)
+
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    m = Y.shape[1]
+    dZ = cache[f'A{L}'] - Y
+
+    for layer in reversed(range(1, L + 1)):
+        A_prev = cache[f'A{layer-1}']
+        W = weights[f'W{layer}']
+        b = weights[f'b{layer}']
+
+        dW = (1 / m) * np.dot(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        if layer > 1:
+            dA_prev = np.dot(W.T, dZ) # Compute gradient w.r.t. previous
+                                      # layers activation (backpropagation step)
+            dA_prev *= cache[f'D{layer-1}']  # Apply dropout mask
+            dA_prev /= keep_prob  # Scale the values
+            dZ = dA_prev * (1 - np.power(A_prev, 2))  # Derivative of tanh
+
+        # Update weights and biases
+        weights[f'W{layer}'] -= alpha * dW
+        weights[f'b{layer}'] -= alpha * db
+
