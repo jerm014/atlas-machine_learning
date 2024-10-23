4ecbb444f6a0f9503390540a6cfad5475ddbae14
Jeremy Mitts <jermitts@gmail.com>
2024-10-22 21:23:12 -0500

    1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -43,15 +43,11 @@ def convolve_grayscale_same(images, kernel):
     output = np.zeros((m, h, w))
 
     # Perform convolution using only two loops (one for images, one for height)
-    for i in range(h):
-        for x in range(w):
+    for hh in range(h):
+        for ww in range(w):
             # Extract the slice of the padded image that corresponds to the
             # current window in height. This slice shape: (kh, w + 2 * pad_w)
-            image_slices = padded_images[:, i:i+kh, x:x+kw]
-
-            # Use sliding_window_view to extract all possible kw-sized windows
-            # along the width. This results in 3D array with shape (w, kh, kw)
-            image_patches = image_slices[:, i:i+kh, x:x+kw]
+            image_slices = padded_images[:, hh:hh+kh, ww:ww+kw]
 
             # Ensure that the number of patches matches the width of the output
             # if image_patches.shape[0] != w:
@@ -60,9 +56,9 @@ def convolve_grayscale_same(images, kernel):
 
             # Perform element-wise multiplication between the kernel and each
             # patch, then sum. This results in a 1D array with shape (w,)
-            conv = np.sum(image_patches * kernel, axis=(1, 2))
+            convolve = np.sum(image_slices * kernel, axis=(1, 2))
 
             # Assign the convolved values to the output
-            output[:, i, x] = conv
+            output[:, hh, ww] = convolve
 
     return output
