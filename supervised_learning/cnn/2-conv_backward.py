af12b2cbe635d13036cf7275aa859d93cf51b15a
Jeremy Mitts <jermitts@gmail.com>
2024-10-29 15:56:56 -0500

    attempt 3

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -40,14 +40,16 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
             h_end = h_start + kh
             w_start = j * sw
             w_end = w_start + kw
+
             for k in range(c_new):
+                dz_mask = dZ[:, i:i+1, j:j+1, k:k+1]
                 dA_prev_pad[:, h_start:h_end, w_start:w_end, :] += \
-                    W[:, :, :, k][np.newaxis, :, :, :] * \
-                    dZ[:, i, j, k][:, np.newaxis, np.newaxis, np.newaxis]
-                dW[:, :, :, k] += np.sum(
+                    np.repeat(np.repeat(dz_mask, kh, axis=1),
+                             kw, axis=2) * W[:, :, :, k:k+1]
+                dW[:, :, :, k:k+1] += np.sum(
                     A_prev_pad[:, h_start:h_end, w_start:w_end, :] *
-                    dZ[:, i, j, k][:, np.newaxis, np.newaxis, np.newaxis],
-                    axis=0)
+                    np.repeat(np.repeat(dz_mask, kh, axis=1),
+                             kw, axis=2), axis=0, keepdims=True)
 
     if padding == 'valid':
         dA_prev = dA_prev_pad
