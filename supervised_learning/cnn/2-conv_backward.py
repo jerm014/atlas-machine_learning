6cc9a6ecad3c8742a26075ff2f7dbc96d5bccb31
Jeremy Mitts <jermitts@gmail.com>
2024-10-29 17:59:21 -0500

    this is so confusing.

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -16,8 +16,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     db = np.zeros_like(b)
 
     if padding == 'same':
-        ph = ((h_new - 1) * sh + kh - h_prev) // 2
-        pw = ((w_new - 1) * sw + kw - w_prev) // 2
+        ph = ((h_new - 1) * sh + kh - h_prev + 1) // 2
+        pw = ((w_new - 1) * sw + kw - w_prev + 1) // 2
     else:
         ph, pw = 0, 0
 
@@ -27,33 +27,22 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                          mode='constant', constant_values=0)
 
     for i in range(m):
-        a_prev_pad = A_prev_pad[i]
-        da_prev_pad = dA_prev_pad[i]
-
         for h in range(h_new):
             for w in range(w_new):
-                vert_start = h * sh
-                vert_end = vert_start + kh
-                horiz_start = w * sw
-                horiz_end = horiz_start + kw
-
-                # Slice the padded input
-                a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
-
-                # Reshape a_slice and dZ to align the dimensions
-                a_slice_reshaped = a_slice[:, :, :, np.newaxis]  # Shape: (kh, kw, c_prev, 1)
-                dZ_reshaped = dZ[i, h, w, :][np.newaxis, np.newaxis, np.newaxis, :]  # Shape: (1, 1, 1, c_new)
-
-                da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += \
-                    np.sum(W * dZ_reshaped, axis=3)
-
-                dW += a_slice_reshaped * dZ_reshaped
-
-        if ph == 0 and pw == 0:
-            dA_prev[i, :, :, :] = da_prev_pad
-        else:
-            dA_prev[i, :, :, :] = da_prev_pad[ph:-ph or None, pw:-pw or None, :]
-
-    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+                for c in range(c_new):
+                    dA_prev[
+                      i,
+                      h*sh:h*sh+kh,
+                      w*sw:w*sw+kw,
+                      :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += dA_prev_pad[
+                      i,
+                      h*sh:h*sh+kh,
+                      w*sw:w*sw+kw,
+                      :] * dZ[i, w, h, c]
+    if padding == 'same':
+        dA_prev = dA_prev[:, ph:-ph, pw:-pw, :]
+    else:
+        dA_prev = dA_prev[:, :h_prev, :w_prev, :]
 
     return dA_prev, dW, db
