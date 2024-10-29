d487cdc8d3a0d273e838d7a8b7142216107a9be9
Jeremy Mitts <jermitts@gmail.com>
2024-10-29 18:05:53 -0500

    this isn't pycodestyle ok, but trying a vetorized approach?

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -4,45 +4,50 @@ import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """documentation here"""
+    """documentation here."""
 
     m, h_new, w_new, c_new = dZ.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
+    kh, kw, _, _ = W.shape
     sh, sw = stride
 
     dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
-    db = np.zeros_like(b)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     if padding == 'same':
-        ph = ((h_new - 1) * sh + kh - h_prev + 1) // 2
-        pw = ((w_new - 1) * sw + kw - w_prev + 1) // 2
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + ((h_prev - 1) * sh + kh - h_prev) % 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + ((w_prev - 1) * sw + kw - w_prev) % 2
     else:
         ph, pw = 0, 0
 
-    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        mode='constant', constant_values=0)
-    dA_prev_pad = np.pad(dA_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                         mode='constant', constant_values=0)
+    A_prev_padded = np.pad(A_prev, ((0,0), (ph, ph), (pw, pw), (0,0)), mode='constant')
+    dA_prev_padded = np.pad(dA_prev, ((0,0), (ph, ph), (pw, pw), (0,0)), mode='constant')
 
-    for i in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    dA_prev[
-                      i,
-                      h*sh:h*sh+kh,
-                      w*sw:w*sw+kw,
-                      :] += W[:, :, :, c] * dZ[i, h, w, c]
-                    dW[:, :, :, c] += dA_prev_pad[
-                      i,
-                      h*sh:h*sh+kh,
-                      w*sw:w*sw+kw,
-                      :] * dZ[i, w, h, c]
-    if padding == 'same':
-        dA_prev = dA_prev[:, ph:-ph, pw:-pw, :]
+    for h in range(h_new):
+        for w in range(w_new):
+
+            vert_start = h * sh
+            vert_end = vert_start + kh
+            horiz_start = w * sw
+            horiz_end = horiz_start + kw
+
+            # Slice A_prev_padded to get the current slice
+            a_slice = A_prev_padded[:, vert_start:vert_end, horiz_start:horiz_end, :]
+
+            # Reshape dZ to align dimensions for broadcasting
+            dZ_slice = dZ[:, h, w, :][:, np.newaxis, np.newaxis, np.newaxis, :]
+
+            # Update gradients for the input
+            dA_prev_padded[:, vert_start:vert_end, horiz_start:horiz_end, :] += np.sum(
+                W[np.newaxis, :, :, :, :] * dZ_slice, axis=4)
+
+            # Update gradients for the kernels
+            dW += np.sum(a_slice[:, :, :, :, np.newaxis] * dZ_slice, axis=0)
+
+    if ph == 0 and pw == 0:
+        dA_prev = dA_prev_padded
     else:
-        dA_prev = dA_prev[:, :h_prev, :w_prev, :]
+        dA_prev = dA_prev_padded[:, ph:-ph, pw:-pw, :]
 
     return dA_prev, dW, db
