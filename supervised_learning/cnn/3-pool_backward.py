62523e3e27e63df81017ef593f1c499a28ebf0ab
Jeremy Mitts <jermitts@gmail.com>
2024-10-30 13:52:55 -0500

    vectorized

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """Performs backpropagation over a pooling layer of a neural network.
+    """
+    Performs backpropagation over a pooling layer of a neural network.
 
     Args:
         dA: numpy.ndarray of shape (m, h_new, w_new, c_new) containing the
@@ -20,42 +21,45 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Returns:
         The partial derivatives with respect to the previous layer (dA_prev).
     """
+    import numpy as np
 
     m, h_new, w_new, c_new = dA.shape
-    _, h_prev, w_prev, c = A_prev.shape
+    m, h_prev, w_prev, c = A_prev.shape  # c_new should be equal to c?
     kh, kw = kernel_shape
     sh, sw = stride
 
-    # init the output gradient to zeros
+    # initialize output gradient to zeros
     dA_prev = np.zeros_like(A_prev)
 
-    for i in range(m):
-        a_prev = A_prev[i]
-        for h in range(h_new):
-            for w in range(w_new):
-                for ch in range(c):
-                    # find corners of the current slice
-                    vert_start = h * sh
-                    vert_end = vert_start + kh
-                    horiz_start = w * sw
-                    horiz_end = horiz_start + kw
+    # transpose dimensions to bring channels to axis 1 for vectorization
+    A_prev = A_prev.transpose(0, 3, 1, 2)  # Shape: (m, c, h_prev, w_prev)
+    dA_prev = dA_prev.transpose(0, 3, 1, 2)  # Shape: (m, c, h_prev, w_prev)
+    dA = dA.transpose(0, 3, 1, 2)  # Shape: (m, c, h_new, w_new)
 
-                    if mode == 'max':
-                        # use mask to identify the max entry
-                        a_slice = a_prev[vert_start:vert_end,
-                                         horiz_start:horiz_end, ch]
-                        mask = (a_slice == np.max(a_slice))
-                        dA_prev[i,
-                                vert_start:vert_end,
-                                horiz_start:horiz_end, ch] += \
-                                    mask * dA[i, h, w, ch]
-                    elif mode == 'avg':
-                        # distribute the gradient
-                        da = dA[i, h, w, ch]
-                        shape = (kh, kw)
-                        average = da / (kh * kw)
-                        dA_prev[i,
-                                vert_start:vert_end,
-                                horiz_start:horiz_end,
-                                ch] += np.ones(shape) * average
-    return
+    for h in range(h_new):
+        for w in range(w_new):
+            # define slice corners
+            vert_start = h * sh
+            vert_end = vert_start + kh
+            horiz_start = w * sw
+            horiz_end = horiz_start + kw
+
+            if mode == 'max':
+                # slice A_prev and create mask of max values
+                a_slice = A_prev[:, :,
+                                 vert_start:vert_end, horiz_start:horiz_end]
+                mask = (a_slice == np.max(a_slice, axis=(2, 3), keepdims=True))
+                # distribute gradient according to mask
+                dA_prev[:, :, vert_start:vert_end, horiz_start:horiz_end] += \
+                    mask * dA[:, :, h:h+1, w:w+1]
+            elif mode == 'avg':
+                # compute gradient for average pooling
+                da = dA[:, :, h:h+1, w:w+1] / (kh * kw)
+                shape = (m, c, kh, kw)
+                # distribute gradient evenly across the poolng window
+                dA_prev[:, :, vert_start:vert_end, horiz_start:horiz_end] += \
+                    np.ones(shape) * da
+
+    # transpose back
+    dA_prev = dA_prev.transpose(0, 2, 3, 1)
+    return dA_prev
