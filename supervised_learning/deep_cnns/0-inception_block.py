7c0f0eaa3e62784dc27cdf0ca11584c5a0a1c68e
Jeremy Mitts <jermitts@gmail.com>
2024-11-06 14:21:49 -0600

    pyodestyle and documentation corrections.

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -5,7 +5,8 @@ from tensorflow import keras as K
 
 def inception_block(A_prev, filters):
     """
-    Builds an inception block as described in "Going Deeper with Convolutions" (2014).
+    Builds an inception block as described in
+    "Going Deeper with Convolutions" (2014).
 
     Args:
      - A_prev:  output fr_om the previous layer
