d87fd6c64707d37f2245a736e408bfe43f556498
Jeremy Mitts <jermitts@gmail.com>
2024-11-14 13:38:52 -0600

    beginning second attempt

--- supervised_learning/transfer_learning/0-transfer-rev-1.py
+++ supervised_learning/transfer_learning/0-transfer-rev-1.py
@@ -6,8 +6,8 @@ from tensorflow import keras as K
 
 
 def preprocess_data(X, Y):
-    """Preprocesses the data for the model."""
-    X_p = K.applications.resnet50.preprocess_input(X)
+    """Preprocesses the data for the model"""
+    X_p = X / 255.0
     Y_p = K.utils.to_categorical(Y, 10)
     return X_p, Y_p
 
@@ -21,30 +21,27 @@ if __name__ == "__main__":
     # Build the base model
     input_shape = (32, 32, 3)
     input_tensor = K.Input(shape=input_shape)
-    resized_images = K.layers.Resizing(224, 224)(input_tensor)
+    resized_images = K.layers.UpSampling2D(size=(7, 7))(input_tensor)
 
-    base_model = K.applications.ResNet50(include_top=False,
-                                         weights='imagenet',
-                                         input_tensor=resized_images)
-
-    for layer in base_model.layers[:100]:
-        layer.trainable = False
+    base_model = K.applications.ResNet50(include_top=False, 
+                                       weights='imagenet',
+                                       input_tensor=resized_images)
 
     x = base_model.output
     x = K.layers.Flatten()(x)
-    x = K.layers.Dense(256, activation='relu')(x)
-    x = K.layers.Dropout(0.5)(x)
+    x = K.layers.Dense(128, activation='relu')(x)
     predictions = K.layers.Dense(10, activation='softmax')(x)
 
     full_model = K.models.Model(inputs=input_tensor, outputs=predictions)
 
-    full_model.compile(optimizer=K.optimizers.Adam(learning_rate=0.001),
-                       loss='categorical_crossentropy',
-                       metrics=['accuracy'])
+    full_model.compile(optimizer=K.optimizers.Adam(),
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
 
+    # Train the model
     full_model.fit(X_train_p, Y_train_p,
-                   validation_data=(X_test_p, Y_test_p),
-                   epochs=7,
-                   batch_size=32)
+                  validation_data=(X_test_p, Y_test_p),
+                  epochs=5, batch_size=32)
 
+    # Save the model
     full_model.save('cifar10.h5')
