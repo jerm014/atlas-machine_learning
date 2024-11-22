5899c4efbcd786274bbfd57ea3c9bcf130bb809e
Jeremy Mitts <jermitts@gmail.com>
2024-11-22 05:26:10 -0600

    corrected main to include some version information.

--- supervised_learning/transfer_learning/main.py
+++ supervised_learning/transfer_learning/main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+import tensorflow as tf
 import tensorflow.keras as K
 preprocess_data = __import__('0-transfer').preprocess_data
+
+# Print TensorFlow and Keras versions
+print(f"TensorFlow version: {tf.__version__}")
+print(f"Keras version: {K.__version__}")
 
 # learning_phase isn't in backend any more. it happens automatically.
 # K.learning_phase = K.backend.learning_phase
 _, (X, Y) = K.datasets.cifar10.load_data()
 X_p, Y_p = preprocess_data(X, Y)
-model = K.models.load_model('cifar10-rev-3.h5')
+model = K.models.load_model('cifar10.h5')
 
 # we're going to get the metrics so we can show everything.
 metrics = model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
