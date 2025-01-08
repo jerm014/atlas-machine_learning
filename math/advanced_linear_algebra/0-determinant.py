57b1b6f38312595cd48f13792e9b2191bb229f40
Jeremy Mitts <jermitts@gmail.com>
2025-01-08 17:50:24 -0600

    0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,1 +1,50 @@
 #!/usr/bin/env python3
+'''make determinant stuff'''
+
+
+def determinant(matrix):
+    '''calculate determinant of matrix'''
+
+    # Check if input is a valid list of lists
+    if not IsListOfLists(matrix):
+        raise TypeError ("matrix must be a list of lists")
+
+    # Check if input is a square matrix
+    if not IsSquareMatrix(matrix):
+        raise ValueError("matrix must be a square matrix")
+
+    if matrix == [[]]:
+        return 1
+
+    n = len(matrix)
+
+    if n == 1:
+        return matrix[0][0]
+
+    if n == 2:
+        # det = (a * d) - (b * c)
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0
+    for j in range(n):
+        minor = [[matrix[i][k] for k in range(n) if k != j] 
+            for i in range(1, n)]
+        # ((-1) ** j) makes the sign flip for every pass of j
+        det += matrix[0][j] * ((-1) ** j) * determinant(minor)
+
+    return det
+
+def IsListOfLists(param):
+    if not isinstance(param, list):
+        return False
+    for item in param:
+        if not isinstance(item, list):
+            return False
+    return True
+
+def IsSquareMatrix(matrix):
+   n = len(matrix)
+   for row in matrix:
+       if len(row) != n:
+           return False
+   return True
