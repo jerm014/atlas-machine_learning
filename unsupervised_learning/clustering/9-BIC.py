1ea30f9a9ec0582415420ff80a32d7da293d42f5
Jeremy Mitts <jermitts@gmail.com>
2025-01-22 20:20:47 -0600

    no for allow

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -26,7 +26,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
           b is a np.ndarray of shape (kmax - kmin + 1,) with BIC values
         or (None, None, None, None) on failure
     """
-    # for n in range(0):
+    # For n in range(0):
     #    try:
     #        print(f"\n{n}-main.py:")
     #        with open(f"{n}-main.py", "r") as file:
