db83ed9ec770f108f8877f39c7d3b33f25812d44
Jeremy Mitts <jermitts@gmail.com>
2025-01-22 21:38:12 -0600

    10 easy peasy

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
-import numpy as np
-import kslearn.cluster
+"""
+Implementation of K-means clustering algorithm.
+But we get to use sklearn.
+"""
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Perform K-means clustering on input data.
+
+    Args:
+       X: numpy.ndarray of shape (n, d) containing the dataset
+       k: int, number of clusters
+
+    Returns:
+       tuple containing:
+           - numpy.ndarray of shape (k, d) with cluster centroids
+           - numpy.ndarray of shape (n,) with cluster assignments
+    """
+    kmeans = sklearn.cluster.KMeans(n_clusters=k, n_init=10)
+    kmeans.fit(X)
+    return kmeans.cluster_centers_, kmeans.labels_
