f099d2f6c4dcd086472a1d2f67a5a2b4a8642737
Jeremy Mitts <jermitts@gmail.com>
2025-01-27 11:22:16 -0600

    pycodestyle

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -50,10 +50,10 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 
     n, d = X.shape
 
-    # if get_main_file() == "./8-main.py":
-        # print(f"kmin: {kmin}")
-        # print(f"kmax: {kmax}")
-        # return None, None, None, None
+    #  if get_main_file() == "./8-main.py":
+        #  print(f"kmin: {kmin}")
+        #  print(f"kmax: {kmax}")
+        #  return None, None, None, None
 
     # If kmax is None, set it to maximum possible clusters: n
     if kmax is None:
