615674295e146a9e912da9a4762f40807833aba5
Jeremy Mitts <jermitts@gmail.com>
2025-02-05 15:03:39 -0600

    task 5 (same validity checks!)

--- unsupervised_learning/hmm/5-backward.py
+++ unsupervised_learning/hmm/5-backward.py
@@ -1,28 +1,70 @@
 #!/usr/bin/env python3
-"""Module for The Backward Algorithm"""
+"""Module for Hidden Markov Model forward, backward, and Viterbi algorithms."""
 import numpy as np
 
 
 def backward(Observation, Emission, Transition, Initial):
-    """Function that performs the backward algorithm for a hidden markov model:
-        Observation is a numpy.ndarray of shape (T,) that contains the index of
-        the observation
-            T is the number of observations
-        Emission is a numpy.ndarray of shape (N, M) containing the emission
-        probability of a specific observation given a hidden state
-            Emission[i, j] is the probability of observing j given the hidden
-            state i
-            N is the number of hidden states
-            M is the number of all possible observations
-        Transition is a 2D numpy.ndarray of shape (N, N) containing the
-        transition probabilities
-            Transition[i, j] is the probability of transitioning rom the hidden
-            state i to j
-        Initial a numpy.ndarray of shape (N, 1) containing the probability of
-        starting in a particular hidden state
-        Returns: P, B, or None, None on failure
-            P is the likelihood of the observations given the model
-            B is a numpy.ndarray of shape (N, T) containing the backward path
-            probabilities
-                B[i, j] is the probability of generating the future
-                observations from hidden state i at time j"""
+    """Calculate backward probabilities for Hidden Markov Model.
+
+    Args:
+        Observation: numpy.ndarray of shape (T,) containing observation
+                     indices
+        Emission:    numpy.ndarray of shape (N, M) containing emission
+                     probabilities
+        Transition:  numpy.ndarray of shape (N, N) containing transition
+                     probabilities
+        Initial:     numpy.ndarray of shape (N, 1) containing starting
+                     probabilities
+
+    Returns:  P, B
+        P: Likelihood of observations given the model
+        B: numpy.ndarray of shape (N, T) containing backward path
+           probabilities
+        or None, None on failure
+    """
+
+    conditions = [
+        isinstance(Observation, np.ndarray),
+        isinstance(Emission, np.ndarray),
+        isinstance(Transition, np.ndarray),
+        isinstance(Initial, np.ndarray)
+    ]
+
+    if not all(conditions):
+        return None, None
+
+    conditions = [
+        len(Transition.shape) == 2,
+        len(Initial.shape) == 2,
+        len(Emission.shape) == 2,
+        Transition.shape[0] == Transition.shape[1],
+        Initial.shape[1] == 1,
+        Initial.shape[0] == Transition.shape[0],
+        np.allclose(np.sum(Transition, axis=1), 1),
+        np.allclose(np.sum(Emission, axis=1), 1),
+        np.allclose(np.sum(Initial), 1)
+    ]
+
+    if not all(conditions):
+        return None, None
+
+    try:
+
+        T = Observation.shape[0]
+        N = Transition.shape[0]
+        B = np.zeros((N, T))
+        B[:, -1] = 1
+
+        for t in range(T - 2, -1, -1):
+            for n in range(N):
+                B[n, t] = np.sum(
+                    Transition[n, :] * Emission[:, Observation[t + 1]] *
+                    B[:, t + 1]
+                )
+
+        P = np.sum(Initial.flatten() * Emission[:, Observation[0]] * B[:, 0])
+
+        return P, B
+
+    except Exception:
+        return None, None
