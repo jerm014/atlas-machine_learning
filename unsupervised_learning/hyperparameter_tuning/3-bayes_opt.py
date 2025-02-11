d50294bb59fc9ad0b4e1c5032fe64446983cf811
Jeremy Mitts <jermitts@gmail.com>
2025-02-11 13:33:52 -0600

    pcs

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -10,16 +10,16 @@ GP = __import__('2-gp').GaussianProcess
 class BayesianOptimization:
     """
     1337 Bayesian optimization for that noiseless 1D Gaussian process swag.
-    
-    This class is straight fire for finding optimal points in ur function space.
-    Get rdy to minimize/maximize like a pr0! Woot!
+
+    This class is straight fire for finding optimal points in ur function
+    space. Get rdy to minimize/maximize like a pr0! Woot!
     """
-    
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, 
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
                  sigma_f=1, xsi=0.01, minimize=True):
         """
         Initialize the sickest Bayesian optimization setup u've ever seen!
-        
+
         Args:
             f:          Ur black-box function 2 be optimized (2 l33t 2 peek
                         inside)
@@ -35,11 +35,12 @@ class BayesianOptimization:
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        
+
         # Generate acquisition sample points
         min_bound, max_bound = bounds
-        self.X_s = np.linspace(min_bound, max_bound, ac_samples
-                              ).reshape(-1, 1)
-        
+        self.X_s = np.linspace(min_bound,
+                               max_bound,
+                               ac_samples).reshape(-1, 1)
+
         self.xsi = xsi
         self.minimize = minimize
