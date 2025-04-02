44bf7287a0bb13a5697dd17867097acd13e45ad7
Jeremy Mitts <jermitts@gmail.com>
2025-04-02 17:45:40 -0500

    about to give up.

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -20,7 +20,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         window=window,
         min_count=min_count,
         negative=negative,
-        sg=sg,
+        sg=not cbow,
         epochs=epochs,
         seed=seed,
         workers=workers
