097e97fb909efc9272d95436354aafb2af632cbd
Jeremy Mitts <jermitts@gmail.com>
2025-04-02 18:09:52 -0500

    testing a build function.

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -7,27 +7,28 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """ Create, build and train a gensim Word2Vec model """
 
-    gensim.utils.np.set_printoptions(precision=7, suppress=False,
-                                     floatmode='maxprec_equal')
-
     # set sg parameter (0 for CBOW, 1 for Skip-gram)
     sg = 0 if cbow else 1
 
     # create Word2Vec model
     model = gensim.models.Word2Vec(
-        sentences=sentences,
-        vector_size=vector_size,
+        size=vector_size,
         window=window,
         min_count=min_count,
+        workers=workers,
+        sg=sg,
         negative=negative,
-        sg=not cbow,
-        epochs=epochs,
-        seed=1,
-        workers=workers
+        seed=seed
     )
+
+    # build vocab
+    model.build_vocab(sentences)
+
     # train the model
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=epochs)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
 
     return model
