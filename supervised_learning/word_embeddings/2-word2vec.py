077a4d61b25850839fb38bb581c5dc9428a6725f
Jeremy Mitts <jermitts@gmail.com>
2025-04-02 16:13:50 -0500

    import rule...

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ gensim """
 import gensim
-from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, 
@@ -19,7 +18,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     sg = 0 if cbow else 1
 
     # create and train the Word2Vec model
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         window=window,
