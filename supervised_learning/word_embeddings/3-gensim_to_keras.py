2e84e17eabaaaece1806dda27d4db7ffdf27f9cf
Jeremy Mitts <jermitts@gmail.com>
2025-04-03 00:28:15 -0500

    task 3

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Convert gensim word2vec model to Keras Embedding """
+import numpy as np
+from tensorflow.keras.layers import Embedding
+
+
+def gensim_to_keras(model):
+    """ Converts trained gensim Word2Vec model to Keras Embedding layer """
+
+    input_dim, output_dim = model.wv.vectors.shape
+
+    embedding_layer = Embedding(
+        input_dim=input_dim,
+        output_dim=output_dim,
+        weights=[model.wv.vectors],
+        trainable=True
+    )
+
+    return embedding_layer
