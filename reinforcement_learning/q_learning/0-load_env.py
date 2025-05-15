6cd39a551a504dcd4bf2bc6d284fcfc1bed10e9c
Jeremy Mitts <jermitts@gmail.com>
2025-05-14 21:25:08 -0500

    pep8 corrections

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,7 +6,7 @@ import gymnasium as gym
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
     Loads the FrozenLake environment from gymnasium with specified parameters.
-    
+
     Args:
         desc (List[List[str]]): A custom description of the map to
             load.
@@ -24,15 +24,16 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
         gym.Env: The FrozenLake environment.
 
     Note:
-        If both desc and map_name are None, a randomly generated 8x8 map will be loaded.
+        If both desc and map_name are None, a randomly generated 8x8 map will
+        be loaded.
         All the arguments are optional and default to None, None, False.
     """
-    
+
     env = gym.make(
         "FrozenLake-v1",
         desc=desc,
         map_name=map_name,
         is_slippery=is_slippery
     )
-    
+
     return env
