7dd40f6af7735425de8f6fae2af34db61e0ee26d
Jeremy Mitts <jermitts@gmail.com>
2025-05-29 09:28:18 -0500

    3/20

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,60 +1,73 @@
 #!/usr/bin/env python3
-"""Module for implementing SARSA algorithm"""
+
+"""This might be the only thing I find a how-to on
+And I don't actually think it's a how to.
+Well Idk which how to that one I was thinking was
+But here's a different one:
+https://github.com/moripiri/
+Reinforcement-Learning-on-FrozenLake/blob/master/Chapter5.ipynb"""
+
 import numpy as np
 
 
-def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
-                  gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
-    """Function that performs SARSA:
-        env: environment instance
-        Q: numpy.ndarray of shape (s, a) containing the Q table
-        lambtha: eligibility trace factor
-        episodes: total number of episodes to train over
-        max_steps: maximum number of steps per episode
-        alpha: learning rate
-        gamma: discount rate
-        epsilon: initial threshold for epsilon greedy
-        min_epsilon: min value that epsilon should decay to
-        epsilon_decay: decay rate for updating epsilon between episodes
+def sarsa_lambtha(
+    env,
+    Q,
+    lambtha,
+    episodes=5000,
+    max_steps=100,
+    alpha=0.1,
+    gamma=0.99,
+    epsilon=1,
+    min_epsilon=0.1,
+    epsilon_decay=0.05,
+):
+    """env - the environment
+    Q - numpy aray of shape (s,a) containing the Q table
+    This is the first time we get a Q table in this assignment
+    lambtha - eligibility trace factor
+    episodes - total epochs/episodes/iterations/whatever
+    max_steps - steps before we mercy kill boiyo
+    alpha - learning rate
+    gamma - discount rate
+    epsilon - initial threshold for epsilon greedy
+    min_epsilon - value of garunteed random
+    epsilon decay - how fast we'll let epsilon decay
+    returns an updated q table."""
 
-        Returns: Q, the update Q table"""
-    n_actions = env.action_space.n
+    eligibility_traces = np.zeros_like(Q)
 
     for episode in range(episodes):
-        E = np.zeros_like(Q)
         state, _ = env.reset()
-        action = epsilon_greedy(Q, state, epsilon, n_actions)
 
-        for _ in range(max_steps):
+        if np.random.rand() < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state])
+
+        eligibility_traces.fill(0)
+
+        for step in range(max_steps):
             next_state, reward, terminated, truncated, _ = env.step(action)
-            done = terminated or truncated
-            next_action = epsilon_greedy(Q, next_state, epsilon, n_actions)
 
-            # TD Error
-            td_error = reward + gamma * Q[next_state, next_action] * \
-                (not done) - Q[state, action]
+            if np.random.rand() < epsilon:
+                next_action = env.action_space.sample()
+            else:
+                next_action = np.argmax(Q[next_state])
 
-            # Replacing traces
-            E[state, :] = 0
-            E[state, action] = 1
+            td_error = reward + gamma * Q[
+                next_state][next_action] - Q[state][action]
 
-            # Update Q and traces
-            Q += alpha * td_error * E
-            E *= gamma * lambtha
+            eligibility_traces[state][action] += 1
+
+            Q += alpha * td_error * eligibility_traces
+            eligibility_traces *= gamma * lambtha
 
             state, action = next_state, next_action
 
-            if done:
+            if terminated or truncated:
                 break
 
-        # Decay epsilon
         epsilon = max(min_epsilon, epsilon * (1 - epsilon_decay))
 
     return Q
-
-
-def epsilon_greedy(Q, state, epsilon, n_actions):
-    """Epsilon-greedy action selection"""
-    if np.random.random() < epsilon:
-        return np.random.randint(n_actions)
-    return np.argmax(Q[state])
