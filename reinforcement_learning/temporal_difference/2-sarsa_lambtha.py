f00aa66f6980114ab282e827d3e6877bee990dd0
Jeremy Mitts <jermitts@gmail.com>
2025-05-29 09:31:18 -0500

    9/20

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python3
-'''task 3'''
-import gym
+"""SARSA(λ)"""
 import numpy as np
 
 
-def sarsa_lambtha(env,
-                  Q,
-                  lambtha,
-                  episodes=5000,
-                  max_steps=100,
-                  alpha=0.1,
-                  gamma=0.99,
-                  epsilon=1,
-                  min_epsilon=0.1,
-                  epsilon_decay=0.05):
-    '''performs SAQRSA'''
+def sarsa_lambtha(env, Q,
+                  lambtha, episodes=5000,
+                  max_steps=100, alpha=0.1,
+                  gamma=0.99, epsilon=1,
+                  min_epsilon=0.1, epsilon_decay=0.05
+                  ):
+    """documentation"""
+    def epsilon_greedy_policy(state, epsilon):
+        if np.random.rand() < epsilon:
+            return np.random.randint(env.action_space.n)
+        else:
+            return np.argmax(Q[state])
+
     for episode in range(episodes):
-        state, _ = env.reset()
-        action = epsilon_greedy(Q, state, epsilon)
-        eligibility_trace = np.zeros_like(Q)
+        state = env.reset()
+        action = epsilon_greedy_policy(state, epsilon)
+        E = np.zeros_like(Q)
 
         for step in range(max_steps):
-            next_state, reward, done, _, _ = env.step(action)
-            next_action = epsilon_greedy(Q, next_state, epsilon)
+            next_state, reward, terminated, truncated, _ = env.step(action)
 
-            delta = reward + gamma * Q[next_state, next_action] - Q[state, action]
-            eligibility_trace[state, action] += 1
+            done = terminated or truncated
 
-            for s in range(Q.shape[0]):
-                for a in range(Q.shape[1]):
-                    Q[s, a] = Q[s, a] + alpha * delta * eligibility_trace[s, a]
-                    eligibility_trace[s, a] *= gamma * lambtha
+            next_action = epsilon_greedy_policy(next_state, epsilon)
 
-            state, action = next_state, next_action
+            delta = reward + gamma * Q[next_state,
+                                       next_action] - Q[state, action]
+            E[state, action] += 1
+
+            Q += alpha * delta * E
+            E *= gamma * lambtha
 
             if done:
                 break
 
-        epsilon = max(min_epsilon, epsilon - epsilon_decay)
+            state, action = next_state, next_action
+
+        epsilon = max(min_epsilon, epsilon * np.exp(-epsilon_decay * episode))
 
     return Q
-
-def epsilon_greedy(Q, state, epsilon):
-    if np.random.rand() < epsilon:
-        return np.random.choice(Q.shape[1])
-    else:
-        return np.argmax(Q[state, :])
-
