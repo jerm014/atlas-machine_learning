80c0ca7a92290d191b60a85b1ad4ef002aff0d62
Jeremy Mitts <jermitts@gmail.com>
2025-06-03 18:49:21 -0500

    rotate

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """
+    Rotates an image by 90 degrees counter-clockwise
+    image: the image
+    returns: the rotated image
+    """
+    return tf.image.rot90(image, k=1)
