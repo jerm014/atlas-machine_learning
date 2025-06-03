2d64a62aa8e96a8913f79fdffd12eaffaed21aa6
Jeremy Mitts <jermitts@gmail.com>
2025-06-03 18:58:54 -0500

    hue

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""change hue of an image"""
+import tensorflow as tf
+
+
+def change_hue(image, delta):
+    """Change the hue of an image"""
+    return tf.image.adjust_hue(image, delta)
