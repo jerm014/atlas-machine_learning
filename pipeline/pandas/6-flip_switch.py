3ddd316c5a37537416bc5fb74b61a004ffdaf04b
Jeremy Mitts <jermitts@gmail.com>
2025-06-21 23:46:22 -0500

    deal with timestamp column

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -18,7 +18,14 @@ def flip_switch(df):
     Returns:
         pandas.DataFrame: The transformed DataFrame, sorted and transposed.
     """
-    sorted_df = df.sort_values(by='Datetime', ascending=False)
+    try:
+        # Sort the DataFrame by the 'Datetime' column in descending order
+        sorted_df = df.sort_values(by='Datetime', ascending=False)
+    except KeyError:
+        # If 'Datetime' column does not exist, try sorting by index?
+        sorted_df = df.sort_index(ascending=False)
+
+    # Transpose the sorted DataFrame
     transformed_df = sorted_df.T
 
     return transformed_df
