e1f985e48d57cc64b04f66ddd5ddea3e1145a9fb
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:30:18 -0500

    task 15

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,3 +1,11 @@
--- test
+-- rank country origins of bands by the number of fans
 
-DESCRIBE metal_bands;
+SELECT
+    origin,
+    SUM(fans) AS nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
