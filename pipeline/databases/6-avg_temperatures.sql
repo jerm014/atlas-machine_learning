bd954f7abe3c3c6464b989b9208ed51ed9eec5d8
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:05:49 -0500

    testing

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,6 +1,8 @@
 -- display the average temperature (Fahrenheit) by city
 -- from the 'temperatures' table, ordered by temperature (descending)
 
+DESCRIBE temperatures;
+
 SELECT city, AVG(temperature) AS avg_temp
 FROM temperatures
 GROUP BY city
