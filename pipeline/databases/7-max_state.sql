5cce8905dab09de6cc750dd63068e71a42d18790
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:11:06 -0500

    task 7

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,3 +1,7 @@
--- test
+-- display the maximum temperature (Fahrenheit) for each state,
+-- ordered by state name in ascending order.
 
-DESCRIBE temperatures;
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC;
