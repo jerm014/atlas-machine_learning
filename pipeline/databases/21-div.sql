1c34f8aa34ac1d25ce20eb334e4b75e2b1e79ab4
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:41:25 -0500

    task 21

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,17 @@
+-- create a function SafeDiv that safely divides two numbers
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END;
+//
+
+DELIMITER ;
