5c821a4e84015c7ff0443678045a4b14e803b5bb
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 23:33:09 -0500

    task 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -8,17 +8,7 @@ methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 def nginx_log_stats():
     """
     Provides some stats about Nginx logs stored in MongoDB.
-    Displays total logs, counts for different HTTP methods,
-    and a specific count for '/status' GET requests.
-    Handles cases where the collection might be empty or connection fails.
     """
-    def print_zero_stats():
-        print("0 logs")
-        print("Methods:")
-        for method in methods:
-            print(f"\tmethod {method}: 0")
-        print("0 status check")
-
     client = None
     client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 
@@ -26,8 +16,6 @@ def nginx_log_stats():
     nginx_collection = db.nginx
 
     total_logs = nginx_collection.count_documents({})
-    if total_logs==0:
-        print_zero_stats()
 
     print(f"{total_logs} logs")
 
