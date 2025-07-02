4a84dcaa2d92ca1b42d808a875d1d3fb2fcf7d58
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:14:27 -0500

    task 8

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,8 @@
--- test
+-- list all shows that have at least one genre linked
+-- It displays the show title and the linked genre ID
+-- Results are sorted by title and then genre ID in ascending order
 
-DESCRIBE tv_genres;
-DESCRIBE tv_show_genres;
-DESCRIBE tv_shows;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
