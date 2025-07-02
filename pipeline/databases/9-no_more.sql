962ea3a1449e5cb3f400b01247a6af3bd73c19c7
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:15:45 -0500

    task 9

---
+++ pipeline/databases/9-no_more.sql
@@ -0,0 +1,9 @@
+-- list all shows that do NOT have any genre linked
+-- It displays the show title and NULL for the genre ID
+-- Results are sorted by title in ascending order
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
