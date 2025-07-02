3e71249f3f9f7f5fc755722a99ac5b5840735741
Jeremy Mitts <jermitts@gmail.com>
2025-07-01 22:20:02 -0500

    task 11

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,3 +1,15 @@
--- test
+-- list all shows from tvshows_rate by their rating
+-- display the show title and the sum of its ratings
+-- sorted: descending order by the rating sum
 
-DESCRIBE tv_show_ratings;
+SELECT
+    tv_shows.title,
+    SUM(tv_show_ratings.rate) AS rating_sum
+FROM
+    tv_shows
+INNER JOIN
+    tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY
+    tv_shows.title
+ORDER BY
+    rating_sum DESC;
